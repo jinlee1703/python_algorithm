a54b97adf2b6325c897327f72c397fddf32cd242
jinu0137 <ljo2379@gmail.com>
2021-12-25 13:34:51 +0900

    2021.12.25.

--- 1157.py
+++ 1157.py
@@ -1,4 +1,14 @@
 # 단어 공부
-# str = list(input().split())
-alphabet = list(ord('z'))
-print(alphabet.count())
+words = input().upper()
+unique_words = list(set(words))     # 입력받은 문자열에서 중복값을 제거
+
+cnt_list = []
+for x in unique_words:
+    cnt = words.count(x)
+    cnt_list.append(cnt)            # 문자별로 count된 횟수를 리스트에 append
+
+if cnt_list.count(max(cnt_list)) > 1:           # count 숫자 최대값이 중복될 경우
+    print('?')
+else:
+    max_index = cnt_list.index(max(cnt_list))   # count 숫자 최대값 인덱스(위치)
+    print(unique_words[max_index])
