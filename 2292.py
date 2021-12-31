5e4f704dac3938bb29392ad5a1e341bcfe70fa91
jinu0137 <ljo2379@gmail.com>
2022-01-01 01:41:40 +0900

    2021.12.31.

--- 2292.py
+++ 2292.py
@@ -1,10 +1,10 @@
 # 벌집
 n = int(input())                # 방 번호 n
-check = 1                       #
-count = 1
-while n > check:
-    count += 1
-    check += (count - 1) * 6
-print(count)
+check = 1                       # 라인별 방 갯수
+count = 1                       # 라인
+while n > check:                # 방 번호 > 방 갯수 (방 찾기)
+    count += 1                  # 다음 라인
+    check += (count - 1) * 6    # 라인별 방 갯수 변경
+print(count)                    # 라인 출력
 
 # 참고 : https://pacific-ocean.tistory.com/102
