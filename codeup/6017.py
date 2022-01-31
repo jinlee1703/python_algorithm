47d76ed9f799a6cbc38e1e04879d120dcd39d351
binaryWoo <binarywooo@gmail.com>
2022-02-01 01:06:49 +0900

    2022.02.01.

---
+++ codeup/6017.py
@@ -0,0 +1,4 @@
+# [기초-입출력] 문장 1개 입력받아 3번 출력하기
+str = input()
+print((str + " ") * 3)
+# print(str, str, str)
---
+++ codeup/6021.py
@@ -0,0 +1,4 @@
+# [기초-입출력] 단어 1개 입력받아 나누어 출력하기
+str = input()
+for x in str:
+    print(x)
