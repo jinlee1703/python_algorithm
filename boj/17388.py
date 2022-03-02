6339ca15a5c5d01e8e9435176651ef60767249af
binaryWoo <binarywooo@gmail.com>
2022-03-03 00:50:30 +0900

    2022.03.03.

---
+++ boj/17388.py
@@ -0,0 +1,11 @@
+# 와글와글 숭고한
+a, b, c = map(int, input().split())
+if a + b + c >= 100:
+    print("OK")
+else:
+    if a == min(a, b, c):
+        print("Soongsil")
+    elif b == min(a, b, c):
+        print("Korea")
+    elif c == min(a, b, c):
+        print("Hanyang")
