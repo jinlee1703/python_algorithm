956fc0a936a8a6c4ce3bb3713a638440fc26c471
binaryWoo <binarywooo@gmail.com>
2022-04-10 05:37:23 +0900

    2022.04.10.

---
+++ boj/2445.py
@@ -0,0 +1,11 @@
+# 별 찍기 - 8
+n = int(input())
+for i in range(1, n):
+    print('*'*i, end='')
+    print(' '*(n*2-i*2), end='')
+    print('*'*i)
+print('*'*(n*2))
+for i in range(n - 1, 0, -1):
+    print('*' * i, end='')
+    print(' ' * (n * 2 - i * 2), end='')
+    print('*' * i)
