de18ba5bf442fc4598709c3c41178a4484eff405
binaryWoo <binarywooo@gmail.com>
2022-05-09 04:42:47 +0900

    date: 2022.05.09.

---
+++ boj/4388.py
@@ -0,0 +1,20 @@
+# 받아올림
+while 1:
+    a, b = input().split()
+    if a == b == '0':
+        break
+
+    if len(a) > len(b):
+        b = (len(a) - len(b)) * '0' + b
+    else:
+        a = (len(b) - len(a)) * '0' + a
+
+    carry_cnt = 0
+    carry = 0
+    for i in range(len(a)-1, -1, -1):
+        if int(a[i]) + int(b[i]) + carry >= 10:
+            carry_cnt += 1
+            carry = 1
+        else:
+            carry = 0
+    print(carry_cnt)
