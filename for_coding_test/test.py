e6349aef36ac67932c82720742934bdeabfeb0b8
binaryWoo <binarywooo@gmail.com>
2022-06-27 18:53:07 +0900

    date: 2022.06.27.

---
+++ for_coding_test/test.py
@@ -0,0 +1,14 @@
+# 문자열 인덱싱
+s = input()
+print(s[0:2])
+print(s[6:-1])
+
+# 문자열 자르기
+arr = s.split(" ")
+print(arr[0], "/", arr[1])
+print(s.split(" ")[0])
+
+# 문자열 합치기
+a = "kakao"
+b = "daum"
+print(a + " " + b)
