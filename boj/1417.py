d13b7e310585a78bb68ec8c79cb33d379ce2b399
Jinwoo Lee <binarywooo@gmail.com>
2022-10-15 23:43:41 +0900

    date: 2022.10.15.

---
+++ boj/1417.py
@@ -0,0 +1,20 @@
+# 국회의원 선거
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+
+vote = list()  # 득표수
+for i in range(0, N):
+    vote.append(int(sys.stdin.readline().rstrip()))
+
+answer = 0
+m = max(vote)
+
+while m != vote[0] or vote.count(m) >= 2:
+    maxIndex = vote[1:].index(max(vote[1:])) + 1
+    vote[0] += 1
+    vote[maxIndex] -= 1
+    answer += 1
+    m = max(vote)
+
+print(answer)
