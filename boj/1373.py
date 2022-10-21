daf40e3a33eba588bcd91f2de733264915589d25
Jinwoo Lee <binarywooo@gmail.com>
2022-10-21 11:02:11 +0900

    date: 2022.10.21.

---
+++ boj/1373.py
@@ -0,0 +1,9 @@
+# 2진수 8진수
+# 2진수 입력받기
+# binary_number = '0b' + input()
+# # 10진수로 변환
+# decimal_number = (binary_number, 2)
+# # 8진수 만들기
+# print(format(decimal_number, 'o'))
+
+print(oct(int(input(),2))[2:])
